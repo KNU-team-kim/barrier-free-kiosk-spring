459e6f1ce2aab2931acdecadc3b818f7a6b79ec6
anjdydhody <a01066444534@gmail.com>
2025-09-17 08:58:51 +0900

    initial commit

---
+++ src/main/java/teamkim/barrier_free_kiosk_spring/BarrierFreeKioskSpringApplication.java
@@ -0,0 +1,13 @@
+package teamkim.barrier_free_kiosk_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrierFreeKioskSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarrierFreeKioskSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teamkim/barrier_free_kiosk_spring/BarrierFreeKioskSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package teamkim.barrier_free_kiosk_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarrierFreeKioskSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
