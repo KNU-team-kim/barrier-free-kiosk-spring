5801d251d3d46e1428f49183de484daa6a0c48bc
anjdydhody <a01066444534@gmail.com>
2025-09-27 20:43:58 +0900

    feat: Swagger 설정 추가
    #3

---
+++ src/main/java/teamkim/barrier_free_kiosk_spring/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package teamkim.barrier_free_kiosk_spring.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Barrier Free Kiosk Server"))
+                .addServersItem(new Server().url("/"));
+    }
+}
