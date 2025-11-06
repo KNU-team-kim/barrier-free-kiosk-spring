7f4ec570999b957997b80abcb752e78a8e0a6762
anjdydhody <a01066444534@gmail.com>
2025-11-07 03:08:34 +0900

    fix: data.sql 수정
    #22

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT IGNORE INTO user
-(name, phone_number, registration_number, gender, sido, sigungu, road_name, building_number, detail_address)
+(name, phone_number, registration_number, gender, sido, sigungu, road_name, main_building_number, sub_building_number, detail_address)
 VALUES
-('장다윤1', '010-4171-7596', '010323-1234567', 'FEMALE', '대구광역시', '북구', '구암로', 21, '101동'),
-('장다윤2', '010-1234-5678', '010323-2345678', 'MALE', '대구광역시', '북구', '경진로', 12, '3층'),
-('장다윤3', '010-9876-5432', '010323-3456789', 'FEMALE', '대전광역시', '유성구', '전민로', 30, '3층');
+('장다윤1', '010-4171-7596', '010323-1234567', 'FEMALE', '대구광역시', '북구', '구암로', 21, null, '101동'),
+('장다윤2', '010-1234-5678', '010323-2345678', 'MALE', '대구광역시', '북구', '경진로', 12, null, '3층'),
+('장다윤3', '010-9876-5432', '010323-3456789', 'FEMALE', '대전광역시', '유성구', '전민로', 30, null, '3층');
 
 COMMIT;
